d1d13b2bec94376cc58518a097dc2e4ed12143c8
“aswinireddy1234” <chegireddyaswinireddy@gmail>
2023-03-14 22:26:59 +0530

    Added: Ability to Create Append Linked List

---
+++ UC3_AbilityAppend.java
@@ -0,0 +1,20 @@
+package linkedlist;
+
+public class UC3_AbilityAppend {
+    public static void main(String[] args) {
+        System.out.println("-----Welcome to DataStructure Program-----");
+        LinkedList<Integer> list=new LinkedList();
+        list.add(70);
+        list.add(30);
+        list.add(56);
+        list.print();
+
+        LinkedList<Integer>list1=new LinkedList<>();
+        list1.add(56);
+        list1.add(30);
+        list1.add(70);
+        list1.print();
+
+
+    }
+}
