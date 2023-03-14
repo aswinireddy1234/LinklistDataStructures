fca2eccbd0f6b1a0b520b9c3fc12385f248c6510
“aswinireddy1234” <chegireddyaswinireddy@gmail>
2023-03-14 22:37:42 +0530

    Added: Ability to Delete Linked List

---
+++ UC5_DeleteLinklist.java
@@ -0,0 +1,27 @@
+package linkedlist;
+
+public class UC5_DeleteLinklist {
+    public static void main(String[] args) {
+        System.out.println("-----Welcome to DataStructure Program-----");
+        LinkedList<Integer> list=new LinkedList();
+        list.add(70);
+        list.add(30);
+        list.add(56);
+        list.print();
+
+        LinkedList<Integer>list1=new LinkedList<>();
+        list1.add(56);
+        list1.add(30);
+        list1.add(70);
+        list1.print();
+
+        LinkedList<Integer>list2=new LinkedList<>();
+        list2.add(56);
+        list2.add(70);
+        list2.searchAndInsert(56,30);
+        list2.print();
+        list2.pop();
+        list2.print();
+    }
+}
+
