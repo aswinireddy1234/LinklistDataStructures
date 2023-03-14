8bf3c740814752a9da22f07ec5238f1adc1312cb
“aswinireddy1234” <chegireddyaswinireddy@gmail>
2023-03-14 22:48:10 +0530

    Added: Ability to Search Linked List

---
+++ UC7_AbilitySearch.java
@@ -0,0 +1,15 @@
+package linkedlist;
+
+public class CreateLinklist {
+    public static void main(String[] args) {
+        System.out.println("-----Welcome to DataStructure Program-----");
+        LinkedList list = new LinkedList();
+
+        list.insertInTheNode(56);
+        list.insertInTheNode(30);
+        list.insertInTheNode(70);
+
+
+        list.searchInTheNode(30);
+    }
+}
