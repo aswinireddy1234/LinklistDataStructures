82a68f7060be1feef11be445d0cc7f90d5614b07
“aswinireddy1234” <chegireddyaswinireddy@gmail>
2023-03-14 22:12:40 +0530

    Added: Creating a Simple Linked List

---
+++ UC1_CreateLinklist.java
@@ -0,0 +1,17 @@
+package linkedlist;
+public class Linklist<T> {
+    public Node<T> head;
+    public Node<T> tail;
+
+    public void push(T data) {
+        Node<T> node = new Node<>();
+        if (head == null) {
+            head = node;
+            tail = node;
+        } else {
+            node.setNext(head);
+            head = node;
+        }
+    }
+}
+
